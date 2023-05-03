1cc27ab7a27653270a9c1ef42dda49e28da2c12c
Jesse Childress <Jesse Childress@JessesHP>
2023-05-03 07:17:08 -0400

    Changed URL to test environment

--- SearchApp/src/configuration/SearchConfig.java
+++ SearchApp/src/configuration/SearchConfig.java
@@ -6,7 +6,10 @@ import CoreConfig.CoreConfig;
 public abstract class SearchConfig extends CoreConfig {
 
 	// Environment URL
-	protected static final String BASE_URL = "https://aretec-search-2my7afm7yq-ue.a.run.app/";
+	protected static final String BASE_URL = "https://test-search-2my7afm7yq-ue.a.run.app/";
+
+	//Production URL
+//	protected static final String BASE_URL = "https://aretec-search-2my7afm7yq-ue.a.run.app/";
 	
 	//Default Locations
 	protected static String DEFAULT_FILE_PATH_FOR_SCREENSHOTS = System.getenv("Eclipse-ScreenshotsLocation");
