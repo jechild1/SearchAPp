7b14cbae2dd970570476dea80f5eddac366a4c32
Jesse Childress <Jesse Childress@JessesHP>
2023-05-18 16:18:13 -0400

    Added initial file upload handling to base

--- SearchApp/src/pageFactories/diSearchBase.java
+++ SearchApp/src/pageFactories/diSearchBase.java
@@ -1,18 +1,93 @@
 package pageFactories;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
 
 import configuration.SearchConfig;
+import utilities.AutomationHelper;
 
 public abstract class diSearchBase extends SearchConfig {
-	
+
 	/*
 	 * Initialize elements of a given page factory
 	 */
 	public void initializeElements() {
 		PageFactory.initElements(driver, this);
 	}
-	
 
+	/**
+	 * Getter method to return a class object for Upload Documents
+	 * 
+	 * @return UploadDocuments
+	 */
+	public UploadDocuments getUploadDocuments() {
+		return new UploadDocuments();
+	}
+
+	/**
+	 * Class that will contain methods that handle the uploading of documents
+	 * 
+	 * @author Jesse Childress
+	 *
+	 */
+	public class UploadDocuments {
+
+		@FindBy(xpath = "//button/span[text() = 'Select Domain']")
+		WebElement selectDomainButton;
+
+		/**
+		 * Clicks the Select Domain button on the Upload Documents modal
+		 */
+		public void clickSelectDomain() {
+			AutomationHelper.printMethodName();
+			selectDomainButton.click();
+		}
+
+		@FindBy(xpath = "//button/span[text() = 'Browse Files']")
+		WebElement browseFilesButton;
+
+		/**
+		 * Clicks the Browse Files button on the Upload Documents modal
+		 */
+		public void clickBrowseFiles() {
+			AutomationHelper.printMethodName();
+			browseFilesButton.click();
+		}
+
+		/**
+		 * Method to upload a file (or multiple files, as indicated by a comma between
+		 * each file name.)
+		 * 
+		 * @param filesToUpload
+		 */
+		public void uploadFile(String filesToUpload) {
+
+			// First, see if the data has multiple files, as separated by a comma.
+			String[] files = filesToUpload.split(",");
+			// Remove the white space from before and after each separate file name
+			for (int i = 0; i < files.length; i++) {
+				files[i] = files[i].trim();
+			}
+
+			// Loop through each item in the array and perform an upload
+
+			for (String currentFile : files) {
+
+				String xpath = "//input[@type='file'][@multiple = '']";
+				WebElement fileUploadObject = driver.findElement(By.xpath(xpath));
+
+				String filePath = generateFullFileNameAndPath(currentFile);
+				// Good to print in the log
+				Reporter.log("File Uploaded: " + filePath, true);
+
+				fileUploadObject.sendKeys(filePath);
+
+			}
+		}
+
+	}
 
 }
