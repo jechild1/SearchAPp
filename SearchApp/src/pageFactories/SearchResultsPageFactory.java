3d028567bb83389403d940168443219675b6a623
Jesse Childress <Jesse Childress@JessesHP>
2023-05-26 16:02:01 -0400

    Created search methods

--- SearchApp/src/pageFactories/SearchResultsPageFactory.java
+++ SearchApp/src/pageFactories/SearchResultsPageFactory.java
@@ -1,5 +1,11 @@
 package pageFactories;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Reporter;
+
+import utilities.AutomationHelper;
+
 /**
  * Page Factory Class to contain methods that interact with objects on the
  * <b>Search Results Page</b>
@@ -7,10 +13,10 @@ package pageFactories;
  * @author jesse.childress
  *
  */
-public class SearchResultsPageFactory extends diSearchMenusPageFactory{
-	
+public class SearchResultsPageFactory extends diSearchMenusPageFactory {
+
 	public static String regexURL = BASE_URL + "search-result" + ".*";
-	
+
 	/**
 	 * Page Constructor: Accepts the WebDriver from the calling page and
 	 * instantiates the elements on the page.
@@ -18,6 +24,72 @@ public class SearchResultsPageFactory extends diSearchMenusPageFactory{
 	public SearchResultsPageFactory() {
 		super(regexURL);
 		waitForPageToLoad();
+	}
+
+	/**
+	 * This method will wait for the Answer text to finished and completed before it
+	 * allows the script to continue.
+	 */
+	private void waitForAnswerGeneration() {
+		AutomationHelper.printMethodName();
+
+		// Grab the xpath of the Answer paragraph:
+		String xpathOfAnswer = "//div[@class = 'content mgBt_10 bullet']/p";
+
+		// Variables to be used in the do/while loop
+		int initialCharacterCount = 0;
+		int waitCharacterCount = 0;
+		int loopCounter = 0;
+
+		do {
+
+			// Grab a reference to the WebElement
+			WebElement answerParagraph = driver.findElement(By.xpath(xpathOfAnswer));
+
+			// Grab the current text of the answer paragraph
+			String initialParagraphText = AutomationHelper.getText(answerParagraph);
+
+			// Count the initial characters in the paragraph
+			initialCharacterCount = initialParagraphText.length();
+
+			// Force the script to wait one second before grabbing a new reference.
+			AutomationHelper.waitSeconds(1);
+
+			// Get new references to objects after a wait so we can compare in the WHILE
+			// below
+			answerParagraph = driver.findElement(By.xpath(xpathOfAnswer));
+			String waitedParagraphText = AutomationHelper.getText(answerParagraph);
+			waitCharacterCount = waitedParagraphText.length();
+
+			loopCounter++;
+
+		} while (initialCharacterCount != waitCharacterCount);
+
+		Reporter.log("Waited on Answer text for: " + loopCounter + " seconds.", true);
+
+	}
+
+	/**
+	 * Method to read the Answer text from the Search Results page.
+	 * <p>
+	 * Because the Answer text is automatically generated, it will require that we
+	 * wait until the text is completed. This method calls waitForAnswerGeneration()
+	 * to ensure the answer is finished before it proceeds to read it.
+	 * 
+	 * @return String
+	 */
+	public String readAnswer() {
+		AutomationHelper.printMethodName();
+
+		// We must wait for the answer to be dynamically generated.
+		waitForAnswerGeneration();
+
+		String xpathOfAnswer = "//div[@class = 'content mgBt_10 bullet']/p";
+
+		WebElement answerWebElement = driver.findElement(By.xpath(xpathOfAnswer));
+
+		return AutomationHelper.getText(answerWebElement);
+
 	}
 
 }
