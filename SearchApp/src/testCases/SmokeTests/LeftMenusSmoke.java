95f92545edef63828892cd11f74b71cb527ca58a
Jesse Childress <Jesse Childress@JessesHP>
2023-06-29 13:17:37 -0400

    Left Menus smoke class changes

--- SearchApp/src/testCases/SmokeTests/LeftMenusSmoke.java
+++ SearchApp/src/testCases/SmokeTests/LeftMenusSmoke.java
@@ -5,8 +5,10 @@ import org.testng.annotations.Test;
 
 import pageFactories.DocumentsPageFactory;
 import pageFactories.SearchPageFactory;
+import pageFactories.SearchResultsPageFactory;
 import testCases.SearchBaseTestScriptConfig;
 import testCases.ModularTests.LoginMod;
+import testCases.ModularTests.SearchMod;
 
 /**
  * Test to login to the system and to ensure that the left menus exist and are
@@ -26,34 +28,32 @@ public class LeftMenusSmoke extends SearchBaseTestScriptConfig {
 
 		SearchPageFactory spPF = new SearchPageFactory();
 
-//		// Perform a simple search
-//		spPF.unselectAllDomains();
-//
-//		SearchMod search = new SearchMod();
-//
-//		// A search is necessary to pull back the page.
-//		search.search("What is software testing?", "DEFAULT", "SQA Testing");
-//
-//		SearchResultsPageFactory srPF = new SearchResultsPageFactory();
-		
-		spPF.clickDocumentsTopLink();
-		
+		// Perform a simple search
+		spPF.unselectAllDomains();
+
+		//Use a modular script to perform a search
+		SearchMod search = new SearchMod();
+		// A search is necessary to pull back the page.
+		search.search("What is software testing?", "SQA Testing");
+
+
+		SearchResultsPageFactory srPF = new SearchResultsPageFactory();
+		srPF.clickDocumentsSlideMenuLink();
 		DocumentsPageFactory documentsPF = new DocumentsPageFactory();
 
 
 		/*
 		 * Assert that the left links are present
 		 */
-		Assert.assertEquals(documentsPF.isSearchResultsPresent(), true, "Left Links - Search Results");
 
-		Assert.assertEquals(documentsPF.isEvaluationYearPresent(), true, "Left Links - Evaluation Year");
-		documentsPF.clickEvaluationYear();
-
-		Assert.assertEquals(documentsPF.isCategoryPresent(), true, "Left Links - Category");
-		documentsPF.clickCategory();
+		Assert.assertEquals(documentsPF.isEvaluationTimePresent(), true, "Left Links - Evaluation Time");
+		documentsPF.clickEvaluationTime();
 
 		Assert.assertEquals(documentsPF.isDocumentsPresent(), true, "Left Links - Document(s)");
 		documentsPF.clickDocuments();
+
+		Assert.assertEquals(documentsPF.isCategoryPresent(), true, "Left Links - Category");
+		documentsPF.clickCategory();
 
 		Assert.assertEquals(documentsPF.isDomainsPresent(), true, "Left Links - Domain(s)");
 		documentsPF.clickDomains();
