77733aa7562c8d0b35314bd05eabb9578590efec
Jesse Childress <Jesse Childress@JessesHP>
2023-06-29 13:10:26 -0400

    Smoke test redesigned as menus changed

--- SearchApp/src/testCases/SmokeTests/SlideMenusSmoke.java
+++ SearchApp/src/testCases/SmokeTests/SlideMenusSmoke.java
@@ -5,58 +5,59 @@ import org.testng.annotations.Test;
 
 import pageFactories.DocumentsPageFactory;
 import pageFactories.SearchPageFactory;
-import pageFactories.SettingsPageFactory;
 import testCases.SearchBaseTestScriptConfig;
 import testCases.ModularTests.LoginMod;
 
 /**
- * Test to login to the system and to ensure that the left menus exist and are
+ * Test to login to the system and to ensure that the slide menus exist and are
  * clickable.
  * 
  * @author Jesse Childress
  *
  */
-public class TopMenusSmoke extends SearchBaseTestScriptConfig {
+public class SlideMenusSmoke extends SearchBaseTestScriptConfig {
 
 	@Test(invocationCount = 1)
 	public void TopMenusSmokeTest() {
 
 		// Login to the system
 		LoginMod loginMod = new LoginMod();
-		loginMod.login("jesse.childress@aretecinc.com", "football3");
+		loginMod.login(USER_NAME, PASSWORD);
 
 		SearchPageFactory spPF = new SearchPageFactory();
-		
-		//The Top menus are accessible without doing a search		
-		//Check for presence of links
-		
-		Assert.assertEquals(spPF.isHomeLinkPresent(), true, "Top Menus - Home Link");
-		Assert.assertEquals(spPF.isDocumentsTopLinkPresent(), true, "Top Menus - Documents Link");
-		Assert.assertEquals(spPF.isAboutLinkPresent(), true, "Top Menus - About Link");
-		Assert.assertEquals(spPF.isSettingsLinkPresent(), true, "Top Menus - Settings Link");
-		Assert.assertEquals(spPF.isLogoutLinkPresent(), true, "Top Menus - Logout Link");
-		
-				
-		//Click through links 
-		
-		//Click Documents
-		spPF.clickDocumentsTopLink();
+
+		// The Top menus are accessible without doing a search
+		// Check for presence of links
+
+		Assert.assertEquals(spPF.isHomeLinkPresent(), true, "Side Menus - Home Link");
+		Assert.assertEquals(spPF.isDocumentsSlideLinkPresent(), true, "Side Menus - Documents Link");
+		Assert.assertEquals(spPF.isQandASlideLinkPresent(), true, "Side Menus - Q&A Link");
+		Assert.assertEquals(spPF.isAboutSlideLinkPresent(), true, "Side Menus - About Link");
+		Assert.assertEquals(spPF.isLogoutSlideLinkPresent(), true, "Top Menus - Logout Link");
+
+		// Click through links
+
+		// Click Documents
+		spPF.clickDocumentsSlideMenuLink();
 		DocumentsPageFactory documentsPF = new DocumentsPageFactory();
-		
-		
-		//Click Home (Which is search page)
-		documentsPF.clickHomeTopLink();
+
+		// Click Home (Which is search page)
+		documentsPF.clickHomeSlideMenuLink();
 		spPF = new SearchPageFactory();
-		
-		//About link doesn't work
-		//spPF.clickAboutLink();
-		
-		//Clicking Settings
-		spPF.clickSettings();
-		SettingsPageFactory settingsPF = new SettingsPageFactory();
-		
-		//Clicking Logout
-		settingsPF.clickLogout();
+
+		// Click Q&A
+		spPF.clickQandASlideLink();
+//		QandAPageFactory qaPF = new QandAPageFactory();
+
+		// Go back home
+		// TODO - There is something about this method that causes a stale reference
+		// with it goes to Q&A Page.
+//		qaPF.clickHomeSlideMenuLink();
+
+		spPF = new SearchPageFactory();
+
+		// Clicking Logout
+		spPF.clickLogoutSlideLink();
 
 	}
 
