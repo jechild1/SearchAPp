6ae62bd3db69056b4f936593b51109b189a0efb1
Jesse Childress <Jesse Childress@JessesHP>
2023-06-29 13:13:05 -0400

    class modifications

--- SearchApp/src/testCases/ModularTests/SearchMod.java
+++ SearchApp/src/testCases/ModularTests/SearchMod.java
@@ -1,7 +1,5 @@
 package testCases.ModularTests;
 
-import java.util.NoSuchElementException;
-
 import org.testng.Assert;
 import org.testng.Reporter;
 
@@ -36,63 +34,24 @@ public class SearchMod extends SearchBaseTestScriptConfig {
 		// Ensure that the text is in the field
 		Assert.assertEquals(searchPF.readSearchField(), searchText);
 
-		// Ensure correct API is selected.
-//		searchPF.setAPIDefault(DEFAULT_API);
-
 		searchPF.clickSearchMagnifyingGlass();
-
 	}
 
 	/**
-	 * This search method enters the passed in search terms and selects the correct
-	 * API to use, corresponding to the passed in API. Selects are DEFAULT and LFQA.
-	 * It does NOT handle domains. By default, it unselects all domains, which searches the entire app.
-	 * 
-	 * @param searchText
-	 * @param api        (DEFAULT or LFQA)
-	 */
-	public void search(String searchText, String api) {
-
-		Reporter.log("Beginning Search Modular Test..." + System.lineSeparator()
-				+ "This is for the main search page from login...", true);
-
-		SearchPageFactory searchPF = new SearchPageFactory();
-
-		searchPF.unselectAllDomains();
-		
-		searchPF.setSearchField(searchText);
-
-		// Ensure that the text is in the field
-		Assert.assertEquals(searchPF.readSearchField(), searchText);
-
-		// Ensure correct API is selected.
-//		if (api.equalsIgnoreCase("DEFAULT")) {
-//			searchPF.setAPIDefault(true);
-//		} else if (api.equalsIgnoreCase("LFQA")) {
-//			searchPF.setLFQADefault(true);
-//		} else {
-//			throw new NoSuchElementException("You must pass in a valid API. Selections are DEFAULT and LFQA");
-//		}
-
-		searchPF.clickSearchMagnifyingGlass();
-
-	}
-
-	/**
-	 * This search method enters the passed in search terms and selects the correct
-	 * API to use, corresponding to the passed in API. Selects are DEFAULT and LFQA.
-	 * It will handle selecting all of the domains that are passed in.
+	 * This search method performs a search with the passed in domain. It will populate the domains as passed in by the String array.
 	 * 
 	 * @param searchText the text that you'd like to search on
-	 * @param API        either LFQA or DEFAULT
 	 * @param Domain(s)    String array that contains domains. Case has to be correct
 	 */
-	public void search(String searchText, String api, String... domains) {
+	public void search(String searchText, String... domains) {
 
 		Reporter.log("Beginning Search Modular Test..." + System.lineSeparator()
 				+ "This is for the main search page from login...", true);
 
 		SearchPageFactory searchPF = new SearchPageFactory();
+		
+		//Unselect domains
+		searchPF.unselectAllDomains();
 
 		// Select the domains that should be used in the search
 		searchPF.selectDomain(domains);
@@ -102,15 +61,6 @@ public class SearchMod extends SearchBaseTestScriptConfig {
 
 		// Ensure that the text is in the field
 		Assert.assertEquals(searchPF.readSearchField(), searchText);
-
-		// Ensure correct API is selected.
-//		if (api.equalsIgnoreCase("DEFAULT")) {
-//			searchPF.setAPIDefault(true);
-//		} else if (api.equalsIgnoreCase("LFQA")) {
-//			searchPF.setLFQADefault(true);
-//		} else {
-//			throw new NoSuchElementException("You must pass in a valid API. Selections are DEFAULT and LFQA");
-//		}
 
 		searchPF.clickSearchMagnifyingGlass();
 
