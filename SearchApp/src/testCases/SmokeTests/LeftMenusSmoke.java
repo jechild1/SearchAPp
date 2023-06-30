0450b84f0069e95fb97a0705371a4aed2cf5dff9
Jesse Childress <Jesse Childress@JessesHP>
2023-06-30 08:03:30 -0400

    Updated number of times test runs

--- SearchApp/src/testCases/SmokeTests/LeftMenusSmoke.java
+++ SearchApp/src/testCases/SmokeTests/LeftMenusSmoke.java
@@ -19,7 +19,7 @@ import testCases.ModularTests.SearchMod;
  */
 public class LeftMenusSmoke extends SearchBaseTestScriptConfig {
 
-	@Test(invocationCount = 5)
+	@Test(invocationCount = 1)
 	public void leftMenusSmoke() {
 
 		// Login to the system
