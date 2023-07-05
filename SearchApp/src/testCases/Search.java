8d2dafe7a9342a3b7fc93a3d7ebdf2da38fd3a46
Jesse Childress <Jesse Childress@JessesHP>
2023-07-05 14:48:46 -0400

    Improvements to Search test case

--- SearchApp/src/testCases/Search.java
+++ SearchApp/src/testCases/Search.java
@@ -1,12 +1,12 @@
 package testCases;
 
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import pageFactories.SearchResultsPageFactory;
 import testCases.ModularTests.LoginMod;
 import testCases.ModularTests.SearchMod;
-import utilities.AutomationHelper;
 
 /**
  * Test to perform a simple search.
@@ -16,28 +16,28 @@ import utilities.AutomationHelper;
  */
 public class Search extends SearchBaseTestScriptConfig {
 
-	@Test (invocationCount = 1)
+	@Test(invocationCount = 1)
 	public void loginSmokeTest() {
 
 		Reporter.log("Beginning test for simple search...", true);
 
+		String searchResults = "Based on the context provided, there is no information suggesting how Aretec can help you save money on taxes. The information provided focuses on the company's expertise in cybersecurity, application development, and data analytics, as well as their experience working with various government agencies. There is no mention of tax-related services or strategies that would directly help you save money on taxes.";
+
 		// Login to the system
 		LoginMod loginMod = new LoginMod();
-		loginMod.login(USER_NAME, PASSWORD);	
-		
-		//Search Page
+		loginMod.login(USER_NAME, PASSWORD);
+
+		// Search Page
 		SearchMod search = new SearchMod();
 		search.search("How can Aretec save money on taxes?", "Proposals");
-		
-		//Search Results
-		SearchResultsPageFactory searchResultsPF = new SearchResultsPageFactory();
-		System.out.println(searchResultsPF.readAnswer());	
-		
-		AutomationHelper.waitSeconds(15);
-		
-		
-		
 
+		// Search Results
+		SearchResultsPageFactory searchResultsPF = new SearchResultsPageFactory();
+		Assert.assertTrue(searchResultsPF.readAnswer().length() > 100, "Search Page - Results returned over 100 characters.");
+		
+		
+		
+	
 	}
 
 }
